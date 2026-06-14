5ce66cfab9e2c2712b9088ce30399c16236f7d14
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-06-15 01:14:10 +0600

    Add SQL query to retrieve upcoming Champions League matches with available status

--- QUERY.sql
+++ QUERY.sql
@@ -1,1 +1,16 @@
 // S M Samiul Hasan
+-- Football Ticket Booking System
+-- Author : S M Samiul Hasan
+
+
+-- Query 1: Retrieve all upcoming football matches belonging to
+--         the 'Champions League' where match status is'Available'
+
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM matches
+WHERE tournament_category = 'Champions League'
+  AND match_status = 'Available';
+ 
